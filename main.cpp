b6fbf84d99af07bde8fd485b5fe81465f66d2458
Jakob Olsson <53495529+maskenjesper@users.noreply.github.com>
2023-03-10 12:26:39 +0100

    Removed the ActionDispatcher since it was useless

--- main.cpp
+++ main.cpp
@@ -3,7 +3,6 @@
 #include "utils/hardwareAPI.h"
 #include "workers/EventReceiver.h"
 #include "workers/ElevatorController.h"
-#include "workers/ActionDispatcher.h"
 
 int main(int argc, char *argv[]) {
     int cabins = argc > 1 ? atoi(argv[1]) : 5;
@@ -11,10 +10,8 @@ int main(int argc, char *argv[]) {
     initHW((char*) "127.0.0.1", 4711);
 
     ElevatorController::init(cabins);
-    ActionDispatcher::init();
     EventReceiver::init();
 
     EventReceiver::quit();
-    ActionDispatcher::quit();
     ElevatorController::quit();
 }
